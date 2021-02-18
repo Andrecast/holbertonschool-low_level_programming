c2b9775292fb0d3584a996340d47ebf2a8d09524
Andrecast <acastrillonpuerta@gmail.com>
2021-02-17 21:45:12 -0500

    Write a function that reverses a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,16 +1,22 @@
 #include "holberton.h"
-#include "2-strlen.c"
-#include "_putchar.c"
 /**
  * rev_string - function that reverses a string
- * _strlen - function that prints the lenght of a string
  * @s: string
  * return: 0
  */
 void rev_string(char *s)
 {
-	int x;
+	char v[10];
+	int a, b;
 
-	for (x = _strlen(s); x > 0; x--)
-		_putchar (s[x - 1]);
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		v[a] = s[a];
+	}
+	a--;
+	for (b = 0; a >= 0; a--)
+	{
+		s[b] = v[a];
+		b++;
+	}
 }
