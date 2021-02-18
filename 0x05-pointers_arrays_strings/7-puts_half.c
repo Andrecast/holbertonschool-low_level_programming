6b0cb5c4093e643ec987e914d129d095851f31a1
Andrecast <acastrillonpuerta@gmail.com>
2021-02-18 12:12:59 -0500

    Write a function that prints half of a string2

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,18 +8,18 @@ void puts_half(char *str)
 {
 	int a;
 
-	for (a = 0; str[a] != '\0'; a++)
+	for (a = 0; str[a] != '\0'; a++) /*recorre el string*/
 	{}
-	if (a % 2 == 0)
+	if (a % 2 == 0) /*si es par*/
 	{
-		for (a /= 2; str[a] != '\0'; a++)
-		{
+		for (a /= 2; str[a] != '\0'; a++) /*tome la longitud del str*/
+		{                                 /*y dividala a la mitad*/
 			_putchar(str[a]);
 		}
 	}
 	else
 	{
-		for ((a = (a - 1) / 2); str[a] != '\0'; a++)
+		for (a = (a - ((a - 1) / 2)); str[a] != '\0'; a++)
 		{
 			_putchar(str[a]);
 		}
