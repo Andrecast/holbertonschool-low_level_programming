43bc0a9ab753d9b830b8375cddcb3e3cfecca52c
Andrecast <acastrillonpuerta@gmail.com>
2021-02-18 21:34:39 -0500

    Write a function that concatenates two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * _strcat - function that concatenate two strings
+ * @dest: array of destiny
+ * @src: string to add
+ * Return: suma
+ */
+char *_strcat(char *dest, char *src)
+{
+	int cont_dest, cont_src;
+
+	for (cont_dest = 0; dest[cont_dest] != '\0'; cont_dest++)
+	{}
+	for (cont_src = 0; src[cont_src] != '\0'; cont_src++)
+	{
+		dest[cont_dest + cont_src] = src[cont_src]; /*concatena y envía a src*/
+	}
+	dest[cont_dest + cont_src] = '\0';
+	return (dest);
+}
+
+
