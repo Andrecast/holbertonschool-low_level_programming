44444c8480a16b56ccd3cb9a7d6f33404e7c71b8
Andrecast <acastrillonpuerta@gmail.com>
2021-02-19 19:37:22 -0500

    Write a function that capitalizes all words of a string1

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,18 +7,22 @@
 char *cap_string(char *str)
 {
 	int a, b;
+	char aux[] = {32, 11, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	char aux[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
-for (a = 0; str[a] != '\0'; a++)
-	{}
-	for (b = 0; aux[b] != '\0'; b++)
+	for (a = 0; str[a] != '\0'; a++)
 	{
-		if (str[0] >= 'a' && str[0] <= 'z')
+		for (b = 0; aux[b] != '\0'; b++)
 		{
-			str[0] = str[0] - 32;
-		}
-		else if (str[a] == aux[b] && str[a + 1] - 32)
-{}
+			if (str[0] >= 97 && str[0] <= 122)/*Lee sólo la letra*/
+			/*inicial de cada palabra de la a a la z*/
+			{
+				str[0] = str[0] - 32;/*restele 32 para volverlo mayuscula*/
+			}
+			else if (str[a] == aux[b] && str[a + 1] >= 97 && str[a + 1] <= 122)
+			{
+				str[a + 1] = str[a + 1] - 32;
+			}
 	}
+}
 	return (str);
 }
