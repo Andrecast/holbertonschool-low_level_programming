4b9135011d98b08b37d908ede6df43262dae7258
Andrecast <acastrillonpuerta@gmail.com>
2021-02-24 20:40:24 -0500

    Write a function that prints a string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _print_rev_recursion - function that prints a string in reverse
+ * @s: string
+ * Return: 0
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == 00)/*si el elemento es el nulo*/
+	{
+		return;/*salga*/
+	}
+	else
+	_print_rev_recursion(s + 1);/*se devuelve*/
+}
