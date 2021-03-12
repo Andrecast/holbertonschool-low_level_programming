138063795e1c8fc1c9d008c050677db43281780b
Andrecast <acastrillonpuerta@gmail.com>
2021-03-11 22:52:12 -0500

    Write a function that prints strings, followed by a new line

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,7 +19,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			else
 			{
 				printf("%s", va_arg(string, char *));
-				if ((i != (n - 1)) && (separator != NULL))
+				if ((i < (n - 1)) && (separator != NULL))
 					printf("%s", separator);
 			}
 			i++;
