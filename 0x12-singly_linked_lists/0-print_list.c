86ce7271895c7abbfbd76f6148f5227e9379f006
Andrecast <acastrillonpuerta@gmail.com>
2021-03-19 14:03:30 -0500

    Write a function that prints all the elements of a list_t list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,12 @@
 #include "lists.h"
+/**
+ * print_list -  Function that prints all the elements of a list_t list
+ * @h: pointer to the structure
+ * Return: the number of nodes
+ */
 
-size_t print_list(const list_t *h)/*h es un puntero q apunta a la estructura list_t*/
+size_t print_list(const list_t *h)
+/*h es un puntero q apunta a la estructura list_t*/
 {
 	int cont = 0;
 
