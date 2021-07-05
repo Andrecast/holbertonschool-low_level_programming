45168bc146411a871d6ffb866bc0a3d26db7ed0a
Andrecast <acastrillonpuerta@gmail.com>
2021-07-04 22:07:03 -0500

    Technical interview preparation: Island perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""5-island_perimeter module"""
+
+
+def island_perimeter(grid):
+    """Returns the perimeter of the island described in grid"""
+    count = 0
+    length = len(grid) - 1
+    width = len(grid[0]) - 1
+
+    for i, j in enumerate(grid):
+        for k, n in enumerate(r):
+            if n == 1:
+                if i == 0 or grid[i - 1][k] != 1:
+                    count += 1
+                if k == 0 or grid[i][k - 1] != 1:
+                    count += 1
+                if k == width or grid[i][k + 1] != 1:
+                    count += 1
+                if i == length or grid[i + 1][k] != 1:
+                    count += 1
+    return count
